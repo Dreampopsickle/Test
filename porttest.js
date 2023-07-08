f1ac50a59096efbd3bc896a066fd0a6cc6d428fb
[CRakotoarisoa@gmail.com] <132526661+Dreampopsickle@users.noreply.github.com>
2023-07-08 03:30:39 -0400

    Made a fucking website, damn son

---
+++ porttest.js
@@ -0,0 +1,8 @@
+// Example: Change background color on click
+document.addEventListener('DOMContentLoaded', function() {
+    var body = document.querySelector('body');
+    body.addEventListener('click', function() {
+      body.style.backgroundColor = '#9FE2BF';
+    });
+  });
+  
