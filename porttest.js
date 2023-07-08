86f35c92591e44c5ee9fa079ced883fdb3219487
[CRakotoarisoa@gmail.com] <132526661+Dreampopsickle@users.noreply.github.com>
2023-07-08 03:42:20 -0400

    random color change added

--- porttest.js
+++ porttest.js
@@ -1,8 +1,25 @@
-// Example: Change background color on click
+// // Example: Change background color on click
+// document.addEventListener('DOMContentLoaded', function() {
+//     var body = document.querySelector('body');
+//     body.addEventListener('click', function() {
+//       body.style.backgroundColor = '#9FE2BF';
+//     });
+//   });
 document.addEventListener('DOMContentLoaded', function() {
-    var body = document.querySelector('body');
-    body.addEventListener('click', function() {
-      body.style.backgroundColor = '#9FE2BF';
-    });
+  var body = document.querySelector('body');
+  body.addEventListener('click', function() {
+    var randomColor = generateRandomColor();
+    body.style.backgroundColor = randomColor;
   });
+
+  function generateRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+});
+
   
